8a0003cefee61ed99d85662f168bf75da611af2f
churkaa <semenovegor546@gmail.com>
2025-06-25 14:08:26 +0300

    n+1 problem

--- src/main/java/my/project/hotel/database/repository/HotelRepository.java
+++ src/main/java/my/project/hotel/database/repository/HotelRepository.java
@@ -1,11 +1,23 @@
 package my.project.hotel.database.repository;
 
+import lombok.NonNull;
 import my.project.hotel.database.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long>, JpaSpecificationExecutor<Hotel> {
+    @Query("""
+    SELECT DISTINCT h FROM Hotel h
+    LEFT JOIN FETCH h.address
+    LEFT JOIN FETCH h.contacts
+    LEFT JOIN FETCH h.arrivalTime
+    LEFT JOIN FETCH h.amenities
+""")
+    @NonNull List<Hotel> findAll();
 }
 
